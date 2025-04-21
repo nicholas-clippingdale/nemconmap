6e0a0c51eb2b993424f0b99fbce0b2e3fa35e594
Nicholas <nick.clippingdale@gmail.com>
2025-04-21 12:06:56 +1000

    Add clean Python module and create NEM tables SQL script

---
+++ src/nemconmap/cleans.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import geopandas as gpd
+from requests import Request
+from owslib.wfs import WebFeatureService
+from nemosis import static_table
+from fuzzywuzzy import process
+
+
+def clean_stations(raw_data_cache):
+    
+    # Extract geo data from WFS server
+    url = 'https://services.ga.gov.au/gis/services/National_Electricity_Infrastructure/MapServer/WFSServer'
+    params = dict(service = 'WFS', version = "2.0.0", request = 'GetFeature', typeName = 'National_Electricity_Infrastructure:Major_Power_Stations')
+    wfs_request_url = Request('GET', url, params = params).prepare().url
+    df_geoservice = gpd.read_file(wfs_request_url)
+    
+    df_geoservice = df_geoservice.loc[~df_geoservice['STATE'].isin(['Western Australia', 'Northern Territory'])]
+
+    
+    # Extract AEMO station data through nemosis
+    df_aemo = static_table('Generators and Scheduled Loads', raw_data_cache)
+
+
+    # Find exact matches
+    df_matched = df_aemo.merge(df_geoservice, how = 'inner', left_on = 'Station Name', right_on = 'POWERSTATION_NAME')
+    # Remove matched stations from source tables to avoid matching again
+    df_aemo_tmp = df_aemo.loc[~df_aemo['Station Name'].isin(df_matched['Station Name'])]
+    df_geoservice_tmp = df_geoservice.loc[~df_geoservice['POWERSTATION_NAME'].isin(df_matched['POWERSTATION_NAME'])]
+
+
+    # Remove the string 'Power Station' then find exact matches
+    df_aemo_tmp.insert(0, 'Station Name - No Power Station', df_aemo_tmp['Station Name'].str.replace(' Power Station', ''))
+    df_geoservice_tmp.insert(0, 'POWERSTATION_NAME - No Power Station', df_geoservice_tmp['POWERSTATION_NAME'].str.replace(' Power Station', ''))
+
+    df_tmp = df_aemo_tmp.merge(df_geoservice_tmp, how = 'inner', left_on = 'Station Name - No Power Station', right_on = 'POWERSTATION_NAME - No Power Station')
+    df_tmp = df_tmp.drop(columns = ['Station Name - No Power Station', 'POWERSTATION_NAME - No Power Station'])
+
+    df_matched = df_matched.append(df_tmp)
+
+    df_aemo_tmp = df_aemo_tmp.loc[~df_aemo_tmp['Station Name'].isin(df_matched['Station Name'])]
+    df_geoservice_tmp = df_geoservice_tmp.loc[~df_geoservice_tmp['POWERSTATION_NAME'].isin(df_matched['POWERSTATION_NAME'])]
+
+
+    # Find the best fuzzywuzzy matches
+    # Use the name without the words Power Station to reduce the number of false positives
+    choices = list(df_geoservice_tmp['POWERSTATION_NAME - No Power Station'])
+
+    for i in df_aemo_tmp['DUID']:
+
+        query = df_aemo_tmp[df_aemo_tmp['DUID'] == i]['Station Name - No Power Station'].item()
+        best_match = process.extractOne(query, choices)
+
+        duid_skip_man = ['CALL_B_1', 'CALL_B_2', '-', 'TIB1', 'TORRB1', 'TORRB2', 'TORRB3', 'TORRB4', 'GB01', 'HBESSG1', 'HBESSL1', 'HBESS1', 'KABANWF1', 'UPPTUMUT', 'WDBESS1', 'BHB1']
+    
+        if best_match[1] >= 90:
+        
+            if i not in duid_skip_man:
+            
+                #print(i, " - ", best_match)
+            
+                df_aemo_tmp_row = df_aemo_tmp[df_aemo_tmp['DUID'] == i]
+                df_geoservice_tmp_row = df_geoservice_tmp[df_geoservice_tmp['POWERSTATION_NAME - No Power Station'] == best_match[0]]
+            
+                df_aemo_tmp_row.insert(0, 'TEMP_JOIN', 'JOINVALUE', allow_duplicates = True)
+                df_geoservice_tmp_row.insert(0, 'TEMP_JOIN', 'JOINVALUE', allow_duplicates = True)
+                df_tmp = df_aemo_tmp_row.merge(df_geoservice_tmp_row, how = 'inner', on = 'TEMP_JOIN')
+                df_tmp = df_tmp.drop(columns = ['Station Name - No Power Station', 'POWERSTATION_NAME - No Power Station', 'TEMP_JOIN'])
+            
+                df_matched = pd.concat([df_matched, df_tmp])
+        
+            #else:
+                #print('Skipping ', i, ' due to manual adjustment')
+
+    df_aemo_tmp = df_aemo_tmp.loc[~df_aemo_tmp['Station Name'].isin(df_matched['Station Name'])]
+    df_geoservice_tmp = df_geoservice_tmp.loc[~df_geoservice_tmp['POWERSTATION_NAME'].isin(df_matched['POWERSTATION_NAME'])]
+
+
+    # Remove all geoservice columns except the geo data
+    df_matched = df_matched.drop(columns = df_geoservice.columns.drop(['gml_id', 'LOCALITY', 'X_COORDINATE', 'Y_COORDINATE', 'geometry']))
+
+
+    df_matched.to_csv(raw_data_cache + 'Stations with Geo Data.csv', index = False)
+
+    return(df_matched)
---
+++ src/nemconmap/create_tables_nem.sql
@@ -0,0 +1,113 @@
+CREATE SCHEMA IF NOT EXISTS nem;
+
+
+CREATE TABLE IF NOT EXISTS nem.gencondata(
+	 effectivedate DATE
+	,versionno NUMERIC(3,0)
+	,genconid VARCHAR(20)
+	,constrainttype VARCHAR(2)
+	,constraintvalue NUMERIC(16,6)
+	,description VARCHAR(256)
+	,genericconstraintweight NUMERIC(16,6)
+	,lastchanged DATE
+	,DISPATCH VARCHAR(1)
+	,predispatch VARCHAR(1)
+	,stpasa VARCHAR(1)
+	,mtpasa VARCHAR(1)
+	,limittype VARCHAR(64)
+	,reason VARCHAR(256)
+	,upload DATE
+	
+	,PRIMARY KEY (effectivedate, versionno, genconid)
+);
+
+--CREATE INDEX IDX_LASTCHANGED
+--ON nem.con_info (LASTCHANGED);
+
+
+CREATE TABLE IF NOT EXISTS nem.genconcoef(
+	 termtype VARCHAR(20)
+	,objectid VARCHAR(12)
+	,effectivedate DATE
+	,versionno NUMERIC(3,0)
+	,genconid VARCHAR(20)
+	,factor NUMERIC(16,6)
+	,lastchanged DATE
+	,bidtype VARCHAR(12)
+	,upload DATE
+
+	,PRIMARY KEY (termtype, objectid, effectivedate, genconid, versionno, bidtype)
+);
+
+--CREATE INDEX IDX_LASTCHANGED
+--ON nem.con_coef (LASTCHANGED);
+
+
+CREATE TABLE IF NOT EXISTS nem.dudetail(
+	 effectivedate DATE
+	,duid VARCHAR(10)
+	,versionno NUMERIC(3,0)
+	,connectionpointid VARCHAR(10)
+	,registeredcapacity NUMERIC(6,0)
+	,agccapability VARCHAR(1)
+	,dispatchtype VARCHAR(20)
+	,maxcapacity NUMERIC(6,0)
+	,starttype VARCHAR(20)
+	,normallyonflag VARCHAR(1)
+	,lastchanged DATE
+	,upload DATE
+
+	,PRIMARY KEY (effectivedate, duid, versionno)
+);
+
+--CREATE INDEX IDX_LASTCHANGED
+--ON nem.dudetail (LASTCHANGED);
+
+
+CREATE TABLE IF NOT EXISTS nem.dudetailsummary(
+	 duid VARCHAR(10)
+	,start_date DATE
+	,end_date DATE
+	,dispatchtype VARCHAR(20)
+	,connectionpointid VARCHAR(10)
+	,regionid VARCHAR(10)
+	,stationid VARCHAR(10)
+	,participantid VARCHAR(10)
+	,lastchanged DATE
+	,transmissionlossfactor NUMERIC(15,5)
+	,starttype VARCHAR(20)
+	,distributionlossfactor NUMERIC(15,5)
+	,schedule_type VARCHAR(20)
+	,max_ramp_rate_up NUMERIC(6,0)
+	,max_ramp_rate_down NUMERIC(6,0)
+	,upload DATE
+
+	,PRIMARY KEY (duid, start_date)
+);
+
+--CREATE INDEX IDX_LASTCHANGED
+--ON nem.dudetailsummary (LASTCHANGED);
+
+
+CREATE TABLE IF NOT EXISTS nem.registeredgenandload(
+	 participant VARCHAR(256)
+	,stationname VARCHAR(256)
+	,region VARCHAR(10)
+	,dispatchtype VARCHAR(20)
+	,category VARCHAR(20)
+	,classification VARCHAR(20)
+	,fuelsourceprimary VARCHAR(256)
+	,fuelsourcedescriptor VARCHAR(256)
+	,technologytypeprimary VARCHAR(256)
+	,technologytypedescriptor VARCHAR(256)
+	,aggregation VARCHAR(1)
+	,duid VARCHAR(10)
+	,gml_id VARCHAR(256)
+	,locality VARCHAR(256)
+	,x_coordinate NUMERIC(3,6)
+	,y_coordinate NUMERIC(3,6)
+	,geometry_point geometry 
+	,upload DATE
+
+	,PRIMARY KEY (duid)
+);
